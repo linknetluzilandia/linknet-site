816eb0c688fba928320447d55dd95311ed6eb690
linknetluzilandia <arthursousa27@gmail.com>
2026-01-23 21:04:42 -0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'linknet-cache-v11'; // 🔥 troque o número sempre que atualizar
+const CACHE_NAME = 'linknet-cache-v2'; // 🔥 troque o número sempre que atualizar
 const URLS_TO_CACHE = [
   '/',
   '/index.html',
@@ -60,12 +60,3 @@ self.addEventListener('fetch', event => {
     })
   );
 });
-
-
-
-
-
-
-
-
-
