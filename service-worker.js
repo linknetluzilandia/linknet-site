4434530afd3755b534eb65629390daba381acd9b
linknetluzilandia <arthursousa27@gmail.com>
2026-01-23 21:54:47 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'linknet-cache-v3'; // 🔥 troque o número sempre que atualizar
+const CACHE_NAME = 'linknet-cache-v4'; // 🔥 troque o número sempre que atualizar
 const URLS_TO_CACHE = [
   '/',
   '/index.html',
@@ -60,4 +60,5 @@ self.addEventListener('fetch', event => {
     })
   );
 });
+
 
